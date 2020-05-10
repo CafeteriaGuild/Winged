319c4cc05fe75fb49f7c55709cda6b064a1c36ae
Adrian Todt <adriantodt.ms@gmail.com>
2020-05-10 19:07:09 -0300

    Officialize the Forward Launcher as a Boost Item

--- src/main/kotlin/net/adriantodt/winged/item/ForwardLauncherItem.kt
+++ src/main/kotlin/net/adriantodt/winged/item/ForwardLauncherItem.kt
@@ -1,15 +1,16 @@
 package net.adriantodt.winged.item
 
+import net.adriantodt.winged.EMPTY_BOOSTER
 import net.adriantodt.winged.boostTheLivingShitOfThisMotherFucker
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
 import net.minecraft.client.item.TooltipContext
-import net.minecraft.entity.EquipmentSlot
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
-import net.minecraft.sound.SoundCategory
+import net.minecraft.sound.SoundCategory.PLAYERS
 import net.minecraft.sound.SoundEvents
+import net.minecraft.sound.SoundEvents.ENTITY_ITEM_BREAK
 import net.minecraft.text.Text
 import net.minecraft.text.TranslatableText
 import net.minecraft.util.Hand
@@ -22,28 +23,15 @@ class ForwardLauncherItem(settings: Settings) : Item(settings) {
         val stack = user.getStackInHand(hand)
 
         if (user.isOnGround) {
-            world.playSound(user, user.blockPos, SoundEvents.BLOCK_PISTON_EXTEND, SoundCategory.PLAYERS, 0.8f, 1.2f)
+            world.playSound(user, user.blockPos, SoundEvents.BLOCK_PISTON_EXTEND, PLAYERS, 0.8f, 1.2f)
             user.boostTheLivingShitOfThisMotherFucker(1.5, 0.1, 0.5)
             user.isOnGround = false
             user.checkFallFlying()
             if (!user.isCreative) {
                 stack.damage += 1
                 if (stack.damage == maxDamage) {
-                    world.playSound(
-                        user,
-                        user.blockPos,
-                        SoundEvents.ENTITY_ITEM_BREAK,
-                        SoundCategory.PLAYERS,
-                        0.8f,
-                        1.2f
-                    )
-                    stack.count = 0
-                    user.sendEquipmentBreakStatus(
-                        when (hand) {
-                            Hand.MAIN_HAND -> EquipmentSlot.MAINHAND
-                            Hand.OFF_HAND -> EquipmentSlot.OFFHAND
-                        }
-                    )
+                    world.playSound(user, user.blockPos, ENTITY_ITEM_BREAK, PLAYERS, 0.8f, 1.2f)
+                    return TypedActionResult.success(ItemStack(EMPTY_BOOSTER))
                 }
             }
             return TypedActionResult.success(stack)
