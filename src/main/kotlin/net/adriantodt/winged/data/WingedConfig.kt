c836e05d88564111ae7e1d504bd5fd4b1150ae6c
Adrian Todt <adriantodt.ms@gmail.com>
2020-05-12 22:34:25 -0300

    Fixed missing text entries

--- src/main/kotlin/net/adriantodt/winged/data/WingedConfig.kt
+++ src/main/kotlin/net/adriantodt/winged/data/WingedConfig.kt
@@ -62,10 +62,10 @@ class WingedConfig : ConfigData {
         var forwardLauncher = ForwardLauncherValues()
 
         @ConfigEntry.Gui.CollapsibleObject
-        var fastBooster = BoosterValues(0.2, 2.5, 0.6, 2)
+        var standardBooster = BoosterValues(0.1, 1.5, 0.5, 4)
 
         @ConfigEntry.Gui.CollapsibleObject
-        var standardBooster = BoosterValues(0.1, 1.5, 0.5, 4)
+        var fastBooster = BoosterValues(0.2, 2.5, 0.6, 2)
 
         @ConfigEntry.Gui.CollapsibleObject
         var slowBooster = BoosterValues(0.1, 0.9, 0.4, 8)
@@ -79,22 +79,22 @@ class WingedConfig : ConfigData {
     )
 
     class ForwardLauncherValues(
-        @ConfigEntry.Gui.Tooltip
+        @ConfigEntry.Gui.Tooltip(count = 2)
         var constantVelocity: Double = 0.9,
-        @ConfigEntry.Gui.Tooltip
+        @ConfigEntry.Gui.Tooltip(count = 2)
         var interpolatingVelocity: Double = 0.1,
-        @ConfigEntry.Gui.Tooltip
+        @ConfigEntry.Gui.Tooltip(count = 2)
         var frictionFactor: Double = 0.4
     )
 
     class BoosterValues(
-        @ConfigEntry.Gui.Tooltip
+        @ConfigEntry.Gui.Tooltip(count = 2)
         var constantVelocity: Double = 0.1,
-        @ConfigEntry.Gui.Tooltip
+        @ConfigEntry.Gui.Tooltip(count = 2)
         var interpolatingVelocity: Double = 1.5,
-        @ConfigEntry.Gui.Tooltip
+        @ConfigEntry.Gui.Tooltip(count = 2)
         var frictionFactor: Double = 0.5,
-        @ConfigEntry.Gui.Tooltip
+        @ConfigEntry.Gui.Tooltip(count = 2)
         var ticksPerDamage: Int = 4
     )
 }
